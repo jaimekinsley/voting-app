27fe59c8402d863c36ceebc8b8161863aaf638cb
Jaime Sanders <jaimelyn517@gmail.com>
2020-06-12 12:33:05 -0700

    when deleting an organization, it also deletes and polls and votes associated with it

--- __tests__/organizations.test.js
+++ __tests__/organizations.test.js
@@ -8,6 +8,7 @@ const app = require('../lib/app');
 const Organization = require('../lib/models/Organization');
 const User = require('../lib/models/User');
 const Membership = require('../lib/models/Membership');
+const Poll = require('../lib/models/Poll');
 
 describe('organization routes', () => {
   beforeAll(async() => {
@@ -150,13 +151,29 @@ describe('organization routes', () => {
       });
   });
 
-  it('deletes an organization by id via DELETE', () => {
-    return Organization.create({
+  it('deletes an organization (and all polls and votes) by id via DELETE', async() => {
+    const organization = await Organization.create({
       title: 'Climate Justice Alliance',
       description: 'Movement building to pivot towards a just transition away from unsustainable energy',
       imageUrl: 'https://climatejusticealliance.org/wp-content/uploads/2019/10/CJA-logo_ESP_600px72dpi-1.png'
-    })
-      .then(organization => request(app).delete(`/api/v1/organizations/${organization._id}`))
+    });
+
+    await Poll.create([{
+      organization: organization._id,
+      title: 'New president election',
+      description: 'At the end of the term, we need to select a new president',
+      options: ['Jaime', 'Carla', 'Sam', 'Louie']
+    },
+    {
+      organization: organization._id,
+      title: 'New treasurer election',
+      description: 'At the end of the term, we need to select a new treasurer',
+      options: ['Steve', 'David', 'Sarah', 'Suzi']
+    }
+    ]);
+
+    return request(app)
+      .delete(`/api/v1/organizations/${organization._id}`)
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.anything(),
@@ -165,6 +182,11 @@ describe('organization routes', () => {
           imageUrl: 'https://climatejusticealliance.org/wp-content/uploads/2019/10/CJA-logo_ESP_600px72dpi-1.png',
           __v: 0
         });
+
+        return Poll.find({ organization: organization._id });
+      })
+      .then(polls => {
+        expect(polls).toEqual([]);
       });
   });
 });
