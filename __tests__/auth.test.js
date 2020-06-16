ce6e0f311585f8c44a900d1fc8b153a230ee024b
Jaime Sanders <jaimelyn517@gmail.com>
2020-06-16 14:32:10 -0700

    gets auth verify user route working and passing the test

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -7,12 +7,16 @@ const connect = require('../lib/utils/connect');
 
 const request = require('supertest');
 const app = require('../lib/app');
-// const User = require('../lib/models/User');
+const User = require('../lib/models/User');
 
 describe('auth routes', () => {
   beforeAll(async() => {
     const uri = await mongod.getUri();
     return connect(uri);
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
   });
 
   afterAll(async() => {
@@ -35,6 +39,42 @@ describe('auth routes', () => {
         expect(res.body).toEqual({
           _id: expect.anything(),
           id: expect.anything(),
+          name: 'Jaime',
+          phone: '503.555.5555',
+          email: 'jaime@jaime.com',
+          communicationMedium: 'email',
+          imageUrl: 'http://myimage.com'
+        });
+      });
+  });
+
+  it('can verify is a user is logged in', async() => {
+    const user = await User.create({
+      name: 'Jaime',
+      password: '12345',
+      phone: '503.555.5555',
+      email: 'jaime@jaime.com',
+      communicationMedium: 'email',
+      imageUrl: 'http://myimage.com'
+    });
+
+    // Use agent here because a request to app wipes out cookies each time
+    const agent = request.agent(app);
+
+    return agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'jaime@jaime.com',
+        password: '12345'
+      })
+      .then(() => {
+        return agent
+          .get('/api/v1/auth/verify');
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: user.id,
+          id: user.id,
           name: 'Jaime',
           phone: '503.555.5555',
           email: 'jaime@jaime.com',
